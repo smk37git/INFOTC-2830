699a722ffdbe97b053f3a489ab3e8b58e61b6d27
smk37git <sdmain3@gmail.com>
2025-11-08 14:23:04 -0600

    Updated data.json; created constructors for classes instead of hard-coding

--- Homework/JS Role-Playing Web Game/main.js
+++ Homework/JS Role-Playing Web Game/main.js
@@ -30,58 +30,84 @@ window.onload = async () => {
 
 // ========== PICK CHARACTER FUNCTION ==========
 function pickCharacter (gameRunning, characterClass, gameData) {
-        if(gameRunning != false && characterClass == 0) {
+    if(gameRunning != false && characterClass == 0) {
 
-            let gameText = document.getElementsByClassName("game-text")[0];
-            gameText.innerHTML = "Begin Game by Selecting Character Type";
+        let gameText = document.getElementsByClassName("game-text")[0];
+        gameText.innerHTML = "Begin Game by Selecting Character Type";
 
-            // Warrior
-            let optionOne = document.getElementById("option-one");
-            optionOne.innerHTML = gameData.playerClasses[0].class;
-            document.getElementById("option-one").onclick = () => {
-                gameText.innerHTML = "You selected " + gameData.playerClasses[0].class;
+        // Warrior
+        let optionOne = document.getElementById("option-one");
+        optionOne.innerHTML = gameData.playerClasses[0].type;
+        document.getElementById("option-one").onclick = () => {
+            gameText.innerHTML = "You selected " + gameData.playerClasses[0].type;
 
-                // Assign character valuess
-                health = gameData.playerClasses[0].health;
-                attackPower = gameData.playerClasses[0].attackPower;
-                defense = gameData.playerClasses[0].defense;
-                level = gameData.playerClasses[0].level;
-            }
+            // Create new Warrior Character
+            const newWarrior = new BaseCharacter(
+                "Warrior", 
+                gameData.playerClasses[0].type,
+                gameData.playerClasses[0].attackPower,
+                gameData.playerClasses[0].defense,
+                gameData.playerClasses[0].level
+            )
+        }
 
-            // Mage
-            let optionTwo = document.getElementById("option-two");
-            optionTwo.innerHTML = gameData.playerClasses[1].class;
-            document.getElementById("option-two").onclick = () => {
-                gameText.innerHTML = "You selected " + gameData.playerClasses[1].class;
-                
-                health = gameData.playerClasses[1].health;
-                attackPower = gameData.playerClasses[1].attackPower;
-                defense = gameData.playerClasses[1].defense;
-                level = gameData.playerClasses[1].level;
-            }
+        // Mage
+        let optionTwo = document.getElementById("option-two");
+        optionTwo.innerHTML = gameData.playerClasses[1].type;
+        document.getElementById("option-two").onclick = () => {
+            gameText.innerHTML = "You selected " + gameData.playerClasses[1].type;
+            
+            // Create new Warrior Character
+            const newMage = new BaseCharacter(
+                "Warrior", 
+                gameData.playerClasses[1].type,
+                gameData.playerClasses[1].attackPower,
+                gameData.playerClasses[1].defense,
+                gameData.playerClasses[1].level
+            )
+        }
 
-            // Thief
-            let optionThree = document.getElementById("option-three");
-            optionThree.innerHTML = gameData.playerClasses[2].class;
-            document.getElementById("option-three").onclick = () => {
-                gameText.innerHTML = "You selected " + gameData.playerClasses[2].class;
+        // Thief
+        let optionThree = document.getElementById("option-three");
+        optionThree.innerHTML = gameData.playerClasses[2].type;
+        document.getElementById("option-three").onclick = () => {
+            gameText.innerHTML = "You selected " + gameData.playerClasses[2].type;
 
-                health = gameData.playerClasses[2].health;
-                attackPower = gameData.playerClasses[2].attackPower;
-                defense = gameData.playerClasses[2].defense;
-                level = gameData.playerClasses[2].level;
-            }
+            // Create new Thief Character
+            const newThief = new BaseCharacter(
+                "Warrior", 
+                gameData.playerClasses[2].type,
+                gameData.playerClasses[2].attackPower,
+                gameData.playerClasses[2].defense,
+                gameData.playerClasses[2].level
+            )
+        }
 
-            // Archer
-            let optionFour = document.getElementById("option-four");
-            optionFour.innerHTML = gameData.playerClasses[3].class;
-            document.getElementById("option-four").onclick = () => {
-                gameText.innerHTML = "You selected " + gameData.playerClasses[3].class;
-                
-                health = gameData.playerClasses[3].health;
-                attackPower = gameData.playerClasses[3].attackPower;
-                defense = gameData.playerClasses[3].defense;
-                level = gameData.playerClasses[3].level;
-            }
+        // Archer
+        let optionFour = document.getElementById("option-four");
+        optionFour.innerHTML = gameData.playerClasses[3].type;
+        document.getElementById("option-four").onclick = () => {
+            gameText.innerHTML = "You selected " + gameData.playerClasses[3].type;
+            
+            // Create new Archer Character
+            const newArcher = new BaseCharacter(
+                "Warrior", 
+                gameData.playerClasses[3].type,
+                gameData.playerClasses[3].attackPower,
+                gameData.playerClasses[3].defense,
+                gameData.playerClasses[3].level
+            )
         }
     }
+}
+
+// ========== CHARACTER CONSTRUCTOR ==========
+class BaseCharacter {
+    constructor(type, health, attackPower, defense, level) {
+        this.type = type;
+        this.health = health;
+        this.attackPower = attackPower;
+        this.defense = defense;
+        this.level = level;
+    }
+}
